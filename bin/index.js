b0349fdfb4603e4f34651770439d6019a26e1f00
Heechul Ryu <ryuhcii@gmail.com>
2023-02-03 15:17:46 -0800

    Revise console logs

--- bin/index.js
+++ bin/index.js
@@ -16,7 +16,7 @@ async function main() {
   const argForConfig = process.argv[2];
   if (argForConfig) {
     configPath = path.resolve(argForConfig);
-    console.log(`reading "${configPath}" to create-svelte`)
+    console.log(`Reading "${configPath}" to create-svelte`)
   }
   else {
     console.warn(`configPath not provided, so reading a default config: "${configPath}" to create-svelte`)
@@ -27,32 +27,17 @@ async function main() {
 
   const argForName = process.argv[3];
   if (argForName) {
-    console.log(`overriding app's name to "${argForName}"`)
+    console.log(`Overriding app's name to "${argForName}"`)
     config.name = argForName;
   }
 
-  console.log(`using this config below to create-svelte`)
+  console.log('Using this config below to create-svelte')
   console.log(config)
 
   await create(config.name, config);
 
-  console.log('your app should be created run command below to verify')
+  console.log('Your app should be created. Run command below to verify')
   console.log(`ls ${config.name}`)
-
-
-  // console.log(
-  //   imported.default
-  // )
-  //
-  // console.log(
-  //   path.resolve(cwd)
-  // )
-  // console.log(
-  //   process.argv
-  // )
-  // console.log(__filename);
-  // console.log(__dirname);
-  // console.log(projectRoot);
 }
 
 main();
